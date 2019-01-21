eeba45b26c5ac9bb1090fbdc352a1f6c27209431
Brent Perry (DAL - ACCF) <burentoperii@gmail.com>
2019-01-21 09:33:12 -0600

    initialize repo

---
+++ RatedR.cs
@@ -0,0 +1,11 @@
+using System;
+
+class RatedR
+{
+  static void Main()
+  {
+    Console.WriteLine("How old are you?");
+    string stringUserAge = Console.ReadLine();
+    int intUserAge = int.Parse(stringUserAge);
+  }
+}
